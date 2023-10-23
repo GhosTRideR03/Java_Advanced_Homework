fab452b5db0bbb651fda3699b975517620c1c054
GhosTRideR03 <96689573+GhosTRideR03@users.noreply.github.com>
2023-10-23 20:03:43 +0200

    Add files via upload

---
+++ HomeworkFive/HomeWorkFive.java
@@ -0,0 +1,101 @@
+package Homeworks.Java_advanced_Homeworks.HomeworkFive;
+
+import java.util.*;
+
+public class HomeWorkFive {
+    public static void main(String[] args) {
+        // Task_1:
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(2);
+        integerList.add(4);
+        integerList.add(6);
+        integerList.add(8);
+        integerList.add(10);
+        listMultiplier(integerList);
+        System.out.println(integerList);
+
+        // Task_2:
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Hello");
+        stringList.add("Banana");
+        stringList.add("Plane");
+        stringList.add("Robotix");
+        List<Integer> lengthList = stringListElementLengthCounter(stringList);
+        System.out.println(lengthList);
+
+        // Task_3:
+        List<Integer> integerListTwo = new ArrayList<>();
+        integerListTwo.add(1);
+        integerListTwo.add(2);
+        integerListTwo.add(3);
+        integerListTwo.add(4);
+        integerListTwo.add(5);
+        integerListTwo.add(6);
+        integerListTwo.add(7);
+        integerListTwo = oddList(integerListTwo);
+        System.out.println(integerListTwo);
+
+        // Task_4:
+        List<String> stringListTwo = new ArrayList<>();
+        stringListTwo.add("Hello");
+        stringListTwo.add("Hello");
+        stringListTwo.add("I'm");
+        stringListTwo.add("Bugs");
+        stringListTwo.add("Bunny");
+        stringListTwo.add("Hello");
+        stringListTwo = noDuplicateList(stringListTwo);
+        System.out.println(stringListTwo);
+
+        List<String> stringListThree = new ArrayList<>();
+        stringListThree.add("Hello");
+        stringListThree.add("Hello");
+        stringListThree.add("I'm");
+        stringListThree.add("Iahimovici");
+        stringListThree.add("Vladislav");
+        stringListThree.add("Hello");
+        stringListThree = noDuplicateListTwo(stringListThree);
+        System.out.println(stringListThree);
+    }
+
+    private static List<String> noDuplicateListTwo(List<String> stringList) {
+//        List<String> noDuplicateList = new ArrayList<>();
+        for (int i = 0; i < stringList.size(); i++) {
+            if (stringList.get(0).equalsIgnoreCase(stringList.get(i))) {
+                stringList.remove(i);
+            }
+        }
+        return stringList;
+    }
+
+
+    private static List<String> noDuplicateList(List<String> stringList) {
+        Set<String> set = new LinkedHashSet<>(stringList);
+        List<String> noDuplicateList = new ArrayList<>(set);
+        return noDuplicateList;
+    }
+
+    private static List<Integer> oddList(List<Integer> integerList) {
+        List<Integer> newIntegerList = new ArrayList<>();
+        for (Integer value : integerList) {
+            if (value % 2 != 0) {
+                newIntegerList.add(value);
+            }
+        }
+        return newIntegerList;
+    }
+
+    private static List<Integer> stringListElementLengthCounter(List<String> stringList) {
+        List<Integer> lengthList = new ArrayList<>();
+        for (String string : stringList) {
+            lengthList.add(string.length());
+        }
+        return lengthList;
+    }
+
+    private static List<Integer> listMultiplier(List<Integer> integerList) {
+        for (int i = 0; i < integerList.size(); i++) {
+            integerList.set(i, integerList.get(i) * 2);
+        }
+        return integerList;
+    }
+}
